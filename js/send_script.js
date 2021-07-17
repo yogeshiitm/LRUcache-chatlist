32fb22eb716f14b298c26e401cab261e3a2f82b5
Yogesh Agarwala <69758664+yogeshiitm@users.noreply.github.com>
2021-07-18 04:54:36 +0530

    temporary solution

--- js/send_script.js
+++ js/send_script.js
@@ -5,6 +5,9 @@ onload = function () {
 
     const chatlist = document.getElementById('chat-list');
     const add = document.getElementById('generate-step');
+    const add2 = document.getElementById('generate-step2');
+    const add3 = document.getElementById('generate-step3');
+    const add4 = document.getElementById('generate-step4');
     const text = document.getElementById('temptext');
 
     const templates = document.getElementsByTagName('template')[0];
@@ -14,6 +17,57 @@ onload = function () {
     let chats = [];
 
     add.onclick = function () {
+        if(Math.random()>0.75 && chats.length > 0){
+            let index = Math.floor(Math.random()*chats.length);
+            let idToDelete = chats[index];
+            chatHandler.deleteMsg(idToDelete);
+            text.innerHTML = "Deleted message from "+chat_names[idToDelete] + "<br>" + text.innerHTML;
+                chats.splice(index, 1);
+        } else{
+            let idOfMsg = Math.floor(Math.random()*7);
+            if(chats.includes(idOfMsg)===false){
+                chats.push(idOfMsg);
+            }
+            chatHandler.newMsg(idOfMsg);
+            text.innerHTML = "New message from "+chat_names[idOfMsg] + "<br>" + text.innerHTML;
+        }
+    };
+
+    add2.onclick = function () {
+        if(Math.random()>0.75 && chats.length > 0){
+            let index = Math.floor(Math.random()*chats.length);
+            let idToDelete = chats[index];
+            chatHandler.deleteMsg(idToDelete);
+            text.innerHTML = "Deleted message from "+chat_names[idToDelete] + "<br>" + text.innerHTML;
+                chats.splice(index, 1);
+        } else{
+            let idOfMsg = Math.floor(Math.random()*7);
+            if(chats.includes(idOfMsg)===false){
+                chats.push(idOfMsg);
+            }
+            chatHandler.newMsg(idOfMsg);
+            text.innerHTML = "New message from "+chat_names[idOfMsg] + "<br>" + text.innerHTML;
+        }
+    };
+
+    add3.onclick = function () {
+        if(Math.random()>0.75 && chats.length > 0){
+            let index = Math.floor(Math.random()*chats.length);
+            let idToDelete = chats[index];
+            chatHandler.deleteMsg(idToDelete);
+            text.innerHTML = "Deleted message from "+chat_names[idToDelete] + "<br>" + text.innerHTML;
+                chats.splice(index, 1);
+        } else{
+            let idOfMsg = Math.floor(Math.random()*7);
+            if(chats.includes(idOfMsg)===false){
+                chats.push(idOfMsg);
+            }
+            chatHandler.newMsg(idOfMsg);
+            text.innerHTML = "New message from "+chat_names[idOfMsg] + "<br>" + text.innerHTML;
+        }
+    };
+
+    add4.onclick = function () {
         if(Math.random()>0.75 && chats.length > 0){
             let index = Math.floor(Math.random()*chats.length);
             let idToDelete = chats[index];
